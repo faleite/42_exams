5cc6aa1beff556d61b4db33a01fb6a3f692fa4ae
Fabricio Leite <fabricio_2310@hotmail.com>
2023-06-26 21:15:15 +0100

    Added ex 00

---
+++ rank_02/00_first_word.c
@@ -0,0 +1,39 @@
+/*
+Assignment name  : first_word
+Expected files   : first_word.c
+Allowed functions: write
+--------------------------------------------------------------------------------
+
+Write a program that takes a string and displays its first word, followed by a
+newline.
+
+A word is a section of string delimited by spaces/tabs or by the start/end of
+the string.
+
+If the number of parameters is not 1, or if there are no words, simply display
+a newline.
+
+Examples:
+
+$> ./first_word "FOR PONY" | cat -e
+FOR$
+$> ./first_word "this        ...    is sparta, then again, maybe    not" | cat -e
+this$
+$> ./first_word "   " | cat -e
+$
+$> ./first_word "a" "b" | cat -e
+$
+$> ./first_word "  lorem,ipsum  " | cat -e
+lorem,ipsum$
+$>
+*/
+
+#include <unistd.h>
+
+/* O numero de parametros tem que ser 1 ou seja argc == 2 (arq + param)*/
+
+/* Enquanto o caracter do param for == space ou tab incremente "i". */
+
+/* Enquanto o caracter do param for != space ou tab, escreva e incremente "i". */
+
+/* Escreva a quebra de linha*/
