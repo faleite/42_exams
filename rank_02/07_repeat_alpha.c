e8ffef9656330e6937e0f574b933ec0da16d0c16
faaraujo <fabricio_2310@hotmail.com>
2023-06-28 22:23:35 +0100

    Added exec 08 level 1

--- rank_02/07_repeat_alpha.c
+++ rank_02/07_repeat_alpha.c
@@ -30,37 +30,10 @@ $
 $>
 */
 
-#include <unistd.h>
+/* #include <> */
 
 /* Function write char x nbr index of char */
-void	putchar_x(char c, int x)
-{
-	while (x > 0)
-	{
-		write(1, &c, 1);
-		--x;
-	}
-}
+
 /* Function write string with repeat of alpha */
-void	repeat_alpha(char *str)
-{
-	while (*str != '\0')
-	{
-		if (*str >= 'a' && *str <= 'z')
-			putchar_x(*str, *str + 1 - 'a');
-		else if (*str >= 'A' && *str <= 'Z')
-			putchar_x(*str, *str + 1 - 'A');
-		else
-			write(1, str, 1);
-		++str;
-	}
-}
 
 /* Program with param */
-int	main(int argc, char **argv)
-{
-	if (argc == 2)
-		repeat_alpha(argv[1]);
-	write(1, "\n", 1);
-	return (0);
-}
