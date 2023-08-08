ea7a2ce190298daf6e092da4c7b628ea43501967
Fabricio Leite <fabricio_2310@hotmail.com>
2023-08-08 20:28:10 +0100

    Work level 2

---
+++ rank_02/2_level/snake_to_camel.c
@@ -0,0 +1,23 @@
+
+#include <unistd.h>
+
+void	snake_to_camel(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] == '_')
+			str[++i] -= 32;
+		write(1, &str[i++], 1);
+	}
+}
+
+int	main(int argc, char *argv[])
+{
+	if (argc == 2)
+		snake_to_camel(argv[1]);
+	write(1, "\n", 1);
+	return (0);
+}
