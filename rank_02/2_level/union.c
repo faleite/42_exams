2970a1bbbaef2e6bd6f0a3ea19afb96136970ceb
Fabricio Araujo <fabricio_2310@hotmail.com>
2023-08-11 17:44:05 +0100

    Create union.c

---
+++ rank_02/2_level/union.c
@@ -0,0 +1,81 @@
+/*
+  Assignment name  : union
+Expected files   : union.c
+Allowed functions: write
+--------------------------------------------------------------------------------
+
+Write a program that takes two strings and displays, without doubles, the
+characters that appear in either one of the strings.
+
+The display will be in the order characters appear in the command line, and
+will be followed by a \n.
+
+If the number of arguments is not 2, the program displays \n.
+
+Example:
+
+$>./union zpadinton "paqefwtdjetyiytjneytjoeyjnejeyj" | cat -e
+zpadintoqefwjy$
+$>./union ddf6vewg64f gtwthgdwthdwfteewhrtag6h4ffdhsd | cat -e
+df6vewg4thras$
+$>./union "rien" "cette phrase ne cache rien" | cat -e
+rienct phas$
+$>./union | cat -e
+$
+$>
+$>./union "rien" | cat -e
+$
+$>
+*/
+
+#include <unistd.h>
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+int	check_double(char *str, char c, int pos)
+{
+	int	i;
+
+	i = 0;
+	while (i < pos)
+	{
+		if (str[i] == c)
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+void	ft_union(char *s1, char *s2)
+{
+	int	i;
+	int	len;
+
+	i = 0;
+	len = ft_strlen(s1);
+	while (s2[i] != '\0')
+		s1[len++] = s2[i++];
+	i = 0;
+	while (s1[i] != '\0')
+	{
+		if (check_double(s1, s1[i], i))
+			write(1, &s1[i], 1);
+		i++;
+	}
+}
+
+int	main(int argc, char *argv[])
+{
+	if (argc == 3)
+		ft_union(argv[1], argv[2]);
+	write(1, "\n", 1);
+	return (0);
+}
