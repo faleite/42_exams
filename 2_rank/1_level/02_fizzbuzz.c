2cd1ebeb510d7e7d5d043654ef3e912750d7a661
Fabricio Araujo <fabricio_2310@hotmail.com>
2023-08-28 20:57:05 +0100

    Update 02_fizzbuzz.c

--- 2_rank/1_level/02_fizzbuzz.c
+++ 2_rank/1_level/02_fizzbuzz.c
@@ -41,6 +41,47 @@ $>
 
 #include <unistd.h>
 
+void	ft_putnbr(int n)
+{
+	if (n < 10)
+		write(1, &"0123456789"[n], 1);
+	else
+	{
+		ft_putnbr(n / 10);
+		ft_putnbr(n % 10);
+	}
+}
+
+void	fizzbuzz(void)
+{
+	int	i;
+
+	i = 1;
+	while (i <= 100)
+	{
+		if (i % 15 == 0)
+			write(1, "fizzbuzz", 8);
+		else if (i % 3 == 0)
+			write(1, "fizz", 4);
+		else if (i % 5 == 0)
+			write(1, "buzz", 4);
+		else
+			ft_putnbr(i);
+		write(1, "\n", 1);
+		i++;
+	}
+}
+
+int	main(void)
+{
+	fizzbuzz();
+	return (0);
+}
+
+
+/********************/
+/* About last exame */
+/********************/
 /* Function for write numbers */
 void	putnbr(int nb)
 {
