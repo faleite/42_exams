81fd60bcd647804e87c6481599d002108b4de53d
Fabricio Araujo <fabricio_2310@hotmail.com>
2023-08-28 21:32:42 +0100

    Update snake_to_camel.c

--- 2_rank/2_level/snake_to_camel.c
+++ 2_rank/2_level/snake_to_camel.c
@@ -1,23 +1,26 @@
-
 #include <unistd.h>
 
-void	snake_to_camel(char *str)
+void	snake_to_camel(char *s)
 {
 	int	i;
 
 	i = 0;
-	while (str[i] != '\0')
+	while (s[i] != '\0')
 	{
-		if (str[i] == '_')
-			str[++i] -= 32;
-		write(1, &str[i++], 1);
+		if (s[i] == '_')
+		{
+			i++;
+			s[i] -= 32;
+		}
+		write(1, &s[i], 1);
+		i++;
 	}
 }
 
-int	main(int argc, char *argv[])
+int	main(int ac, char *av[])
 {
-	if (argc == 2)
-		snake_to_camel(argv[1]);
+	if (ac == 2)
+		snake_to_camel(av[1]);
 	write(1, "\n", 1);
 	return (0);
 }
