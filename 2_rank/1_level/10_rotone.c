addddc571ace685a29ba7fa407fee934d0c05c70
Fabricio Araujo <fabricio_2310@hotmail.com>
2023-08-28 19:02:30 +0100

    Update 10_rotone.c

--- 2_rank/1_level/10_rotone.c
+++ 2_rank/1_level/10_rotone.c
@@ -31,6 +31,34 @@ $>
 
 #include <unistd.h>
 
+void	rotone(char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if ((s[i] >= 'A' && s[i] <= 'Y') || (s[i] >= 'a' && s[i] <= 'y'))
+			s[i] += 1;
+		else if (s[i] == 'Z' || s[i] == 'z')
+			s[i] -= 25;
+		write(1, &s[i], 1);
+		i++;
+	}
+}
+
+int	main(int ac, char *av[])
+{
+	if (ac == 2)
+		rotone(av[1]);
+	write(1, "\n", 1);
+	return (0);
+}
+
+
+/*******************/
+/* Abut last exame */
+/*******************/
 void	putchar(char c)
 {
 	write(1, &c, 1);
