eabaed3a9841c9aadb6a72c823d137e850c1bc34
Fabricio Araujo <fabricio_2310@hotmail.com>
2023-08-28 21:12:01 +0100

    Update 12_ulstr.c

--- 2_rank/1_level/12_ulstr.c
+++ 2_rank/1_level/12_ulstr.c
@@ -24,3 +24,30 @@ $
 */
 
 /* This time de answer is not 42, is 32! */
+
+
+#include <unistd.h>
+
+void	ulstr(char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 'A' && s[i] <= 'Z')
+			s[i] += 32;
+		else if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] -= 32;
+		write(1, &s[i], 1);
+		i++;
+	}
+}
+
+int	main(int ac, char *av[])
+{
+	if (ac == 2)
+		ulstr(av[1]);
+	write(1, "\n", 1);
+	return (0);
+}
