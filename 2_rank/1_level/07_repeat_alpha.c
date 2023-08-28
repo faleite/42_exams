b77d8871c6d5aafdf772ac9c6225da0d23116b84
Fabricio Araujo <fabricio_2310@hotmail.com>
2023-08-28 18:46:15 +0100

    Update 07_repeat_alpha.c

--- 2_rank/1_level/07_repeat_alpha.c
+++ 2_rank/1_level/07_repeat_alpha.c
@@ -37,3 +37,39 @@ $>
 /* Function write string with repeat of alpha */
 
 /* Program with param */
+
+#include <unistd.h>
+
+void	putchar(char c, int n)
+{
+	while (n)
+	{
+		write(1, &c, 1);
+		n--;
+	}
+}
+
+void	repeat_alpha(char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 'A' && s[i] <= 'Z')
+			putchar(s[i], ((s[i] - 'A') + 1));
+		else if (s[i] >= 'a' && s[i] <= 'z')
+			putchar(s[i], ((s[i] - 'a') + 1));
+		else
+			write(1, &s[i], 1);
+		i++;
+	}
+}
+
+int	main(int ac, char *av[])
+{
+	if (ac == 2)
+		repeat_alpha(av[1]);
+	write(1, "\n", 1);
+	return (0);
+}
