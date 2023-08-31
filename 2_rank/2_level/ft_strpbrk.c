a22ec38ab56015aff878b069779cb6724b810107
Fabricio Araujo <fabricio_2310@hotmail.com>
2023-08-31 17:27:24 +0100

    Update ft_strpbrk.c

--- 2_rank/2_level/ft_strpbrk.c
+++ 2_rank/2_level/ft_strpbrk.c
@@ -17,6 +17,27 @@ char	*ft_strpbrk(const char *s1, const char *s2);
 
 char	*ft_strpbrk(const char *s1, const char *s2)
 {
+	int		j;
+
+	while (*s1 != '\0')
+	{
+		j = 0;
+		while (s2[j] != '\0')
+		{
+			if (*s1 == s2[j])
+				return ((char *)s1);
+			j++;
+		}
+		s1++;
+	}
+	return (0);
+}
+
+/********************/
+/* About last exame */
+/********************/
+char	*ft_strpbrk(const char *s1, const char *s2)
+{
 	int i;
 	char *str;
 
