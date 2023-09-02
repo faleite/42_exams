bb7b4cb10b86f5bf3395e8ccef42ed829d1f94f3
Fabricio Araujo <fabricio_2310@hotmail.com>
2023-09-02 20:34:56 +0100

    Update ft_split.c

--- 2_rank/4_level/ft_split.c
+++ 2_rank/4_level/ft_split.c
@@ -5,6 +5,40 @@
 #define WD_NUM 1000
 #define WD_LEN 1000
 
+/************/
+/* Option 1 */ 
+/************/
+char	**ft_split(char *str)
+{
+	char	**arr;
+	int		j;
+	int		k;
+
+	arr = (char **)malloc(sizeof(char *) * WD_NUM);
+	while (*str == ' ' || *str == '\t' || *str == '\n')
+		str++;
+	j = 0;
+	while (*str != '\0')
+	{
+		if (*str > 32)
+		{
+			k = 0;
+			arr[j] = (char *)malloc(sizeof(char) * WD_LEN);
+			while (*str > 32)
+				arr[j][k++] = *str++;
+			arr[j][k] = '\0';
+			j++;
+		}
+		else
+			str++;
+	}
+	arr[j] = 0;
+	return (arr);
+}
+
+/************/
+/* Option 2 */ 
+/************/
 void	copy_word(char **tab, char *str)
 {
 	int	i;
