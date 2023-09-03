19dd28d354abc3b78d70f3da5796db2ee6519379
Fabricio Araujo <fabricio_2310@hotmail.com>
2023-09-03 16:17:14 +0100

    Update ft_itoa.c

--- 2_rank/4_level/ft_itoa.c
+++ 2_rank/4_level/ft_itoa.c
@@ -1,53 +1,73 @@
+/*
+Assignment name  : ft_itoa
+Expected files   : ft_itoa.c
+Allowed functions: malloc
+--------------------------------------------------------------------------------
 
+Write a function that takes an int and converts it to a null-terminated string.
+The function returns the result in a char array that you must allocate.
+
+Your function must be declared as follows:
+
+char	*ft_itoa(int nbr);
+*/
 #include <stdlib.h>
 #include <stdio.h>
 
-int	len_nbr(int nb)
+int	len_nbr(int n)
 {
-	int	i;
+	int	len;
 
-	i = 0;
-	while (nb)
+	len = 0;
+	if (n <= 0)
 	{
-		nb /= 10;
-		i++;
+		len++;
+		n *= -1;
 	}
-	return (i);
+	while (n)
+	{
+		n /= 10;
+		len++;
+	}
+	return (len);
 }
 
 char	*ft_itoa(int nbr)
 {
 	int		len;
-	char	*arr;
-	int		cpynu;
+	char	*str;
 
-	if (nbr == -2147483648)
-		return ("-2147483648");
-	cpynu = nbr;
-	len = len_nbr(nbr);
-	if (nbr < 0)
-	{
-		cpynu *= -1;
-		len++;
-	}
 	if (nbr == 0)
 		return ("0");
-	arr = (char *)malloc(sizeof(char) * len + 1);
-	if (!arr)
+	if (nbr == -2147483648)
+		return ("-2147483648");
+	len = len_nbr(nbr);
+	str = (char *)malloc(sizeof(char) * len + 1);
+	if (!str)
 		return (NULL);
 	if (nbr < 0)
-		arr[0] = '-';
-	arr[len] = '\0';
-	while (cpynu)
 	{
-		arr[--len] = (cpynu % 10) + 48;
-		cpynu /= 10;
+		nbr = -nbr;
+		str[0] = '-';
 	}
-	return (arr);
+	str[--len] = '\0';
+	while (nbr)
+	{
+		str[len] = (nbr % 10) + '0';
+		nbr /= 10;
+		len--;
+	}
+	return (str);
 }
-
-// int	main(void)
-// {
-// 	printf("%s", ft_itoa((0)));
-// 	return (0);
-// }
+/*
+int	main(void)
+{
+	printf("%s\n", ft_itoa(12345));
+	printf("%s\n", ft_itoa(0));
+	printf("%s\n", ft_itoa(-12345));
+	printf("%s\n", ft_itoa(2147483647));
+	printf("%s\n", ft_itoa(-2147483647));
+	printf("%s\n", ft_itoa(-2147483648));
+	printf("%s\n", ft_itoa(2147483648));
+}
+*/
