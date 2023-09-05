19619294c2bddf3329b72179b4b2a0ded5552ffa
Fabricio Araujo <fabricio_2310@hotmail.com>
2023-09-05 20:37:55 +0100

    Update max.c

--- 2_rank/2_level/max.c
+++ 2_rank/2_level/max.c
@@ -1,4 +1,20 @@
-
+/** MAX
+* Assignment name  : max
+* Expected files   : max.c
+* Allowed functions:
+* --------------------------------------------------------------------------------
+*
+* Write the following function:
+*
+* int		max(int* tab, unsigned int len);
+*
+* The first parameter is an array of int, the second is the number of elements in
+* the array.
+*
+* The function returns the largest number found in the array.
+*
+* If the array is empty, the function returns 0.
+*/
 #include <stdio.h>
 
 int	max(int *tab, unsigned int len)
